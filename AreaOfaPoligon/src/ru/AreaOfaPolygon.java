74a85a7f8e3a8c38fcded590e1ea0e551b2c310c
georg <man240893@mail.ru>
2013-04-12 12:21:35 +0400

    OLOLOpISSHpISH

---
+++ AreaOfaPoligon/src/ru/AreaOfaPolygon.java
@@ -0,0 +1,21 @@
+package ru;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class AreaOfaPolygon {
+    public static Frame frame;
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                frame = new Frame();
+                frame.toFront();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
---
+++ AreaOfaPoligon/src/ru/Panel.java
@@ -0,0 +1,21 @@
+package ru;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+
+class Panel extends JPanel
+{
+    public static void paintSquare(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g; //???????? ??????? ?????????? ???????
+
+        Rectangle2D field = new Rectangle2D.Double(Frame.Ax, Frame.Ay, 10, 10);
+        g2.setPaint(Color.white);
+        g2.fill(field);
+    }
+    public static void paintLine(Graphics g, int Ax, int Ay , int Bx, int By)
+    {
+        g.drawLine(Ax, Ay, Bx, By);
+    }
+}
